35408c289158a8796d9bf4491dc34e389626dd2b
LAIBA KAMAL <laibakamal003@gmail.com>
2022-09-23 09:27:08 +0500

    Update StudentListAdapter.java

--- SqliteDatabasePostVacLec01/app/src/main/java/com/example/sqlitedatabasepostvaclec01/StudentListAdapter.java
+++ SqliteDatabasePostVacLec01/app/src/main/java/com/example/sqlitedatabasepostvaclec01/StudentListAdapter.java
@@ -19,7 +19,6 @@ import java.util.List;
 
 public class StudentListAdapter extends ArrayAdapter<StudentModel> {
 
-
     public StudentListAdapter(@NonNull Context context, @NonNull List<StudentModel> objects) {
         super(context, 0, objects);
     }
@@ -28,27 +27,22 @@ public class StudentListAdapter extends ArrayAdapter<StudentModel> {
     @Override
     public View getView(int position, @NonNull View convertView, @NonNull ViewGroup parent) {
         StudentModel s = getItem(position);
-        convertView = LayoutInflater.from(getContext()).inflate(R.layout.view_student, parent,
-                false);
+        convertView = LayoutInflater.from(getContext()).inflate(R.layout.view_student, parent, false);
         TextView name = convertView.findViewById(R.id.name);
         TextView rollNo = convertView.findViewById(R.id.rollno);
         TextView enrolled = convertView.findViewById(R.id.isActive);
         name.setText(String.valueOf(s.getName()));
         rollNo.setText(String.valueOf(s.getRollNmber()));
-
         if (s.isEnroll() == 0) {
             enrolled.setText("Not Enrolled");
         } else if (s.isEnroll() == 1) {
             enrolled.setText("Enrolled");
         }
-
         if (position % 2 == 1) {
             convertView.setBackgroundColor(Color.rgb(255, 216, 216));
         } else {
             convertView.setBackgroundColor(Color.rgb(216, 253, 255));
         }
-
-
         return convertView;
     }
 
